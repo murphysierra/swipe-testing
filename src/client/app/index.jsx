6abcd996dea86010b1a9192f96cd8ad635c11b5c
Sierra <sierra@campika.com>
2016-03-08 12:25:31 -0700

    Add a bunch of nested stuff for testing

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -8,11 +8,12 @@ import {render} from 'react-dom';
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {displayedComponent : 'awesomeComponent'};
+		this.state = { displayedComponent: 'awesomeComponent'};
 		this.onClick = this.onClick.bind(this);
 	}
 
 	onClick() {
+    //this.refs.ReactSwipe.props.slide();
 		if (this.state.displayedComponent === 'awesomeComponent') {
 			this.setState({displayedComponent: 'anotherComponent'});
 		} else if (this.state.displayedComponent === 'anotherComponent') {
@@ -22,7 +23,27 @@ class App extends React.Component {
     }
 	}
 
-	render () {
+  createGiantNestedCrap() {
+    let divs = [];
+    let nestedDivs = [];
+    let i;
+    for (i = 0; i < 20; i++) {
+      nestedDivs.push(<div>
+        SOME TEXT
+      </div>);
+    }
+    for (i = 0; i < 100; i++) {
+      divs.push(<div>
+        <br /><a>Thing number {i}</a><br />
+        <img src={'https://img.new.livestream.com/events/00000000002aa744/638fdca0-994e-45df-be81-a5e32adf334d_170x255.jpg'}/>
+        <div>{nestedDivs}</div>
+        </div>);
+    }
+    console.log(divs);
+    return divs;
+  }
+
+	render() {
     const components = {
       awesomeComponent: <AwesomeComponent />,
       anotherComponent: <AnotherComponent />,
@@ -30,16 +51,17 @@ class App extends React.Component {
     };
 
     const displayedKitten = components[this.state.displayedComponent];
+    const divs = this.createGiantNestedCrap();
 
     return (
           <div>
             <p> Hello Frikin React!</p>
             <div><button onClick={this.onClick.bind(this)}>Kittens?</button></div>
             {displayedKitten}
-            <ReactSwipe continuous={false} key={this.state.displayedComponent}>
-                <div>{displayedKitten}</div>
-                <div><AnotherComponent /></div>
-                <div><AThirdComponent /></div>
+            <ReactSwipe ref="ReactSwipe"
+                        continuous={true}
+                        key={this.state.displayedComponent}>
+              {divs}
             </ReactSwipe>
           </div>
     );
